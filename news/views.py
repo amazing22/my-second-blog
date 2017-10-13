bda2bcf3b4ae66a90650fe67f3fb4c15421f87b1
Your Name <you@example.com>
2017-10-14 00:51:05 +0900

    css변경

--- news/views.py
+++ news/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-
 from .models import Article
 
 def year_archive(request, year):
@@ -7,10 +6,10 @@ def year_archive(request, year):
     context = {'year': year, 'article_list': a_list}
     return render(request, 'news/year_archive.html', context)
 
-def month_archive(request, month):
-    a_list = Article.objects.filter(pub_date__month=month)
-    context = {'month': month, 'article_list': a_list}
-    return render(request, 'news/month_archive.html', context)
 
-def article_detail(request, detail):
-    return render(request, 'news/article_detail.html', context)
+def month_archive(request):
+    return render(request, 'news/month_archive.html',{})
+
+def article_detail(request, pk):
+    a_list = get_object_or_404(Article, pk=pk)
+    return render(request, 'news/detail.html',{'a_list' : a_list})
